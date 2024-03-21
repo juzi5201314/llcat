e206663379e7fc52dddc2346a7e4ac45ae37fff9
soeur <juzi201314@gmail.com>
2024-03-21 22:34:56 +0800

    [new] 编写一个关于StmtBlock的benchmark

---
+++ llcat-parser/benches/parse_bench.rs
@@ -0,0 +1,14 @@
+#![feature(test)]
+
+extern crate test;
+
+use llcat_parser::parser::Parser;
+use test::{black_box, Bencher};
+
+#[bench]
+fn bench_parse_block(b: &mut Bencher) {
+    let parser = Parser::new("{6 * 6} - ({1 + 2; 3 + 4} + 5)").without_print_error();
+    b.iter(|| {
+        let _ = black_box(parser.parse());
+    });
+}
