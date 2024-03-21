2d85b46a6f675f349e5d9905bc7c36d2e64cdb2c
soeur <juzi201314@gmail.com>
2024-03-21 23:31:44 +0800

    [feat] 使用一个包装结构为SmallVec实现chumsky::Container

--- llcat-parser/src/small_vec.rs
+++ llcat-parser/src/small_vec.rs
@@ -1,4 +1,4 @@
-use smallvec::SmallVec;
+use smallvec::{Array, SmallVec};
 
 macro_rules! _def {
     ($($num:literal),*) => {
@@ -9,3 +9,21 @@ macro_rules! _def {
 }
 
 _def!(1, 2, 3, 4, 6, 8, 10, 12, 16, 20, 24, 32, 48, 64, 96, 128, 256, 512, 1024);
+
+pub struct ContainerWrapper<A: Array>(pub SmallVec<A>);
+
+impl<A> chumsky::container::Container<A::Item> for ContainerWrapper<A> where A: Array {
+    fn push(&mut self, item: A::Item) {
+        self.0.push(item)
+    }
+    
+    fn with_capacity(n: usize) -> Self {
+        ContainerWrapper(SmallVec::with_capacity(n))
+    }
+}
+
+impl<A> Default for ContainerWrapper<A> where A: Array {
+    fn default() -> Self {
+        ContainerWrapper(SmallVec::new())
+    }
+}
