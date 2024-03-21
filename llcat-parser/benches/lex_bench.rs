d929412bc391b54fc8bcad7992eaf786ff50bdf9
soeur <juzi201314@gmail.com>
2024-03-21 18:53:35 +0800

    [fix] 修复lex bench的错误

--- llcat-parser/benches/lex_bench.rs
+++ llcat-parser/benches/lex_bench.rs
@@ -1,26 +1,38 @@
 #![feature(test)]
 
+use chumsky::span::SimpleSpan;
+use llcat_parser::token::{lexer, Token};
+use smallvec::SmallVec;
 use test::{black_box, Bencher};
 
 extern crate test;
 
+type Consume = SmallVec<[(Token, SimpleSpan); 8]>;
+
 #[bench]
 fn bench_lex_str(b: &mut Bencher) {
     b.iter(|| {
-        let _ = black_box(llcat_parser::parser::parse_src("ident127335cb323292b8127335cb323292b8"));
+        let _ = black_box(lexer("ident").collect::<Consume>());
+    });
+}
+
+#[bench]
+fn bench_lex_str_on_heap(b: &mut Bencher) {
+    b.iter(|| {
+        let _ = black_box(lexer("ident127335cb323292b8127335cb323292b8").collect::<Consume>());
     });
 }
 
 #[bench]
 fn bench_lex_int(b: &mut Bencher) {
     b.iter(|| {
-        let _ = black_box(llcat_parser::parser::parse_src("12345678"));
+        let _ = black_box(lexer("12345678").collect::<Consume>());
     });
 }
 
 #[bench]
 fn bench_lex_int_on_heap(b: &mut Bencher) {
     b.iter(|| {
-        let _ = black_box(llcat_parser::parser::parse_src("123456789"));
+        let _ = black_box(lexer("123456789").collect::<Consume>());
     });
 }
